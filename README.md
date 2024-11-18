4aa87a2f1db029899601e6d10ec056618ec4add2
t-ski <t.schiepanski@gmx.de>
2024-11-18 20:44:05 +0100

    Implement SVG to TTF iconfont emitter

--- README.md
+++ README.md
@@ -1,3 +1,82 @@
 ## [rJS](https://rapidjs.org) Plugin: Iconfont
 
-`TODO`
+Direct iconfont from SVG icon files.
+
+### Install
+
+``` console
+npm i rapidjs-org/plugin--iconfont
+```
+
+<sub><code>__rjs.plugin.json</code></sub>
+``` json
+{
+  "package": "@plugins.rapidjs.org/iconfont"
+}
+```
+
+### Use
+
+```
+└─ /src …
+   └─ /iconfont
+      ├─ __rjs.plugin.json
+      └─ /icons
+         ├─ 1-plus.svg
+         ├─ 2-chevron.svg
+         └─ 3-cross.svg
+```
+
+<sub><code>public/style.css</code></sub>
+
+``` css
+@font-face {
+  font-family: "iconfont";
+  src: url("/iconfont.ttf");
+}
+
+.icon::before {
+  font-family: "iconfont";
+}
+
+.icon--plus::before {
+  content: "\E000"
+}
+.icon--chevron::before {
+  content: "\E001"
+}
+.icon--cross::before {
+  content: "\E002"
+}
+```
+
+<sub><code>public/index.html</code></sub>
+
+``` html
+<html>
+  <head>
+    <link rel="stylesheet" href="/style.css">
+  </head>
+  <body>
+    <a href="/new" class="icon icon--plus"></a>
+  </body>
+</html>
+```
+
+### Configure
+
+By default, the plugin emits the iconfont to `/iconfont.ttf`. An alternative can be specified via `outPath`.  
+
+<sub><code>__rjs.plugin.json</code></sub>
+``` json
+{
+  "package": "@plugins.rapidjs.org/iconfont",
+  "config": {
+    "outPath": "assets/fonts/my-iconfont"
+  }
+}
+```
+
+##
+
+<sub>&copy; Thassilo Martin Schiepanski</sub>
--- plugin/plugin.js
+++ plugin/plugin.js
@@ -1,5 +1,45 @@
-module.exports = (rJS, pluginFilesystem, pluginConfig, pluginOptions) => {
-    // TODO: Implement
-    
-    return [];	// Array of rJS.File instances
-};
+import { createReadStream } from "fs";
+import { join } from "path";
+
+import svg2ttf from "svg2ttf";
+import { SVGIcons2SVGFontStream } from "svgicons2svgfont";
+
+
+export default async function(rJS, filesystem, config, __, $PATH) {
+    return new Promise((resolve, reject) => {
+        const fontStream = new SVGIcons2SVGFontStream({
+            fontName: "Iconfont",
+        })
+        .on("finish", async () => {
+            const chunks = [];
+            for await (const chunk of fontStream) {
+                chunks.push(Buffer.from(chunk));
+            }
+
+            resolve(
+                new rJS.File(
+                    (config.outPath ?? "/iconfont.ttf").replace(/(\.ttf)?$/i, ".ttf"),
+                    svg2ttf(Buffer.concat(chunks).toString("utf-8")).buffer
+                )
+            );
+        })
+        .on("error", reject);
+
+        let i = 0;
+        filesystem
+        .get("/icons")
+        .traverse(file => {
+            if(file.extension !== "svg") return;
+            
+            const unicode = String.fromCharCode(0xE000 + i++);
+            const glyph = createReadStream(join($PATH, "icons", `${file.name}.svg`));
+            glyph.metadata = {
+                unicode: [ unicode ],
+                name: `icon-${i}`
+            };
+            fontStream.write(glyph);
+        });
+        
+        fontStream.end();
+    });
+}
